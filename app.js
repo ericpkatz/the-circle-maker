b0460c369e67441977c22f7e0c6382df3da7db19
ericpkatz <ericpkatz@gmail.com>
2022-10-26 10:55:22 -0400

    created the app

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+//let form = document.getElementsByTagName('form')[0];
+let form = document.querySelector('form');
+let input = document.querySelector('input');
+let container = document.querySelector('#container');
+console.log(container);
+form.addEventListener('submit', function(ev){
+  ev.preventDefault();
+  let size = input.value;
+  let div = document.createElement('div');
+  div.style.width = size + 'rem';
+  div.style.height = size + 'rem';
+  div.classList.add('circle');
+  container.appendChild(div);
+});
